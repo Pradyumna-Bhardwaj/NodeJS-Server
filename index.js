51ff1faa92b3cfaf4b9e77c8bba75a5fe3332f00
Pradyumna <bhardwaj.pradyumn.pb@gmail.com>
2024-09-13 15:47:54 +0530

    Express integrated

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
+const express = require("express");
 
-const myServer = http.createServer((req, res) => {
-    if(req.url === "favicon.ico") return res.end();
+const app = express();
 
-    const log = `${Date.now()}: \t ${req.method}  ${req.url}\t  New Req Received\n`;
-    const myURL = url.parse(req.url, true);
+app.get("/", (req, res) => {
+    return res.send("Home Page");
+})
+app.get("/about", (req, res) => {
+    return res.send(`Hi ${req.query.myname}, this is a server built by Morty`);
+})
 
-    if (req.url !== "/favicon.ico"){
-    fs.appendFile("log.txt", log, (err, data) => {
-        switch(myURL.pathname){
-            case "/":
-                res.end("HomePage");
-                break;
-            case "/about":
-                const username = myURL.query.myname;
-                res.end(`hi ${username}, I am Server build through Morty.`);
-                break; 
-            default:
-                res.end("404: Page not found");
-        }
-    })};
-});
-myServer.listen(8000, ()=>console.log("server started"));
+app.listen(8000, () => console.log("server started"));
+
+// function myhandler(req, res){
+//     if(req.url === "favicon.ico") return res.end();
+
+//     const log = `${Date.now()}: \t ${req.method}  ${req.url}\t  New Req Received\n`;
+//     const myURL = url.parse(req.url, true);
+
+//     if (req.url !== "/favicon.ico"){
+//     fs.appendFile("log.txt", log, (err, data) => {
+//         switch(myURL.pathname){
+//             case "/":
+//                 res.end("HomePage");
+//                 break;
+//             case "/about":
+//                 const username = myURL.query.myname;
+//                 res.end(`hi ${username}, I am Server build through Morty.`);
+//                 break; 
+//             default:
+//                 res.end("404: Page not found");
+//         }
+//     })};
+// }
+
+// const myServer = http.createServer(app);
+// myServer.listen(8000, ()=>console.log("server started"));
