22d7219ed315685ddd2d8a3b638aa00d7e948c30
Pradyumna <bhardwaj.pradyumn.pb@gmail.com>
2024-09-13 13:32:56 +0530

    query parsing added

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const url = require("url");
 
 const myServer = http.createServer((req, res) => {
     if(req.url === "favicon.ico") return res.end();
-    const log = `${Date.now()}: \t${req.url}\t  New Req Received\n`;
-    const myURL = url.parse(req.url);
+
+    const log = `${Date.now()}: \t ${req.method}  ${req.url}\t  New Req Received\n`;
+    const myURL = url.parse(req.url, true);
+
     if (req.url !== "/favicon.ico"){
     fs.appendFile("log.txt", log, (err, data) => {
         switch(myURL.pathname){
@@ -13,7 +15,8 @@ const myServer = http.createServer((req, res) => {
                 res.end("HomePage");
                 break;
             case "/about":
-                res.end("I am Server build through Morty.");
+                const username = myURL.query.myname;
+                res.end(`hi ${username}, I am Server build through Morty.`);
                 break; 
             default:
                 res.end("404: Page not found");
